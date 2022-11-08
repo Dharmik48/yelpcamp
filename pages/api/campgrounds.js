1b6d7220b44cee0e987360290c09e3addc15af29
dharmik48 <hingudharmik4@gmail.com>
2022-11-08 19:52:21 +0530

    feat: creates POST rest api

---
+++ pages/api/campgrounds.js
@@ -0,0 +1,16 @@
+import connectDB from '../../util/mongo'
+import Campground from '../../models/Campground'
+
+export default async function handler(req, res) {
+  // Connect to databse
+  await connectDB()
+
+  // Check the method of req
+  // If the method is POST
+  if (req.method === 'POST') {
+    // Create a new Campground
+    const campground = await Campground.create(req.body)
+    // Send the data back with status 200
+    res.status(200).json(campground)
+  }
+}

5d2a9bcf1aa42869dfd8e27a5e0665723f46690a
dharmik48 <hingudharmik4@gmail.com>
2022-11-08 20:16:50 +0530

    chore: moves campgrounds api into its own dir

--- pages/api/campgrounds/index.js
+++ pages/api/campgrounds/index.js
@@ -1,5 +1,5 @@
-import connectDB from '../../util/mongo'
-import Campground from '../../models/Campground'
+import connectDB from '../../../util/mongo'
+import Campground from '../../../models/Campground'
 
 export default async function handler(req, res) {
   // Connect to databse

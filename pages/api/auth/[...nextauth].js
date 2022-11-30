5eb19f233d390fdb63907a3626d084043a970f80
dharmik48 <hingudharmik4@gmail.com>
2022-11-30 11:32:32 +0530

    feat: adds facebook login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,10 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
     CredentialsProvider({
       name: 'Credentials',
       async authorize(credentials, req) {
@@ -19,8 +23,9 @@ export default NextAuth({
 
         // Check if the user exists
         const user = await User.findOne({ email: credentials.email })
-        // If does not exists then throw error
-        if (!user) throw new Error('Invalid email or password!')
+        // If does not exists or exists but the auth type is not email/pass then throw error
+        if (!user || (user && user.auth_type !== 'credentials'))
+          throw new Error('Invalid email or password!')
         // If user exists then check password
         const isPasswordCorrect = await compare(
           credentials.password,

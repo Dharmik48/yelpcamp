ec89c10c9182e3505d561726e63fa2783bda745f
dharmik48 <hingudharmik4@gmail.com>
2023-02-06 19:11:15 +0530

    feat: adds timestapms to user

--- models/User.js
+++ models/User.js
@@ -1,27 +1,30 @@
 import { Schema, model, models } from 'mongoose'
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      min: [8, 'Password must be atleast 8 characters'],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    auth_type: {
+      type: [String],
+      enum: ['credentials', 'google', 'facebook'],
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    min: [8, 'Password must be atleast 8 characters'],
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  auth_type: {
-    type: [String],
-    enum: ['credentials', 'google', 'facebook'],
-    required: true,
-  },
-})
+  { timestamps: true }
+)
 
 export default models.User || model('User', userSchema)

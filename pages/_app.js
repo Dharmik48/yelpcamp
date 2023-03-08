d35156b1fe68c5e418ea7f2f56529bbea6168859
dharmik48 <hingudharmik4@gmail.com>
2023-03-08 18:05:25 +0530

    feat: adds progress bar for route change loading

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,20 @@ import { SessionProvider } from 'next-auth/react'
 import Loader from '../components/Loader'
 import Router from 'next/router'
 import { useState } from 'react'
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false)
 
-  Router.events.on('routeChangeStart', () => setLoading(true))
-  Router.events.on('routeChangeComplete', () => setLoading(false))
+  Router.events.on('routeChangeStart', () => {
+    setLoading(true)
+    NProgress.start()
+  })
+  Router.events.on('routeChangeComplete', () => {
+    setLoading(false)
+    NProgress.done()
+  })
 
   return (
     <SessionProvider session={pageProps.session}>

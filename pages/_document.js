a02ecd1ac2fec7c591ff398ddadd9c28adcfd852
dharmik48 <hingudharmik4@gmail.com>
2023-03-18 19:25:26 +0530

    feat: adds dark/light mode favicons

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,16 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Volkhov:wght@700&display=swap'
           rel='stylesheet'
         />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+          media='(prefers-color-scheme: light)'
+        />
+        <link
+          rel='icon'
+          href='/favicon-dark.ico'
+          media='(prefers-color-scheme: dark)'
+        />
       </Head>
       <body className='bg-primaryBg'>
         <Main />

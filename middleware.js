0ec848547799c9569022c0af19c186271638cd2c
dharmik48 <hingudharmik4@gmail.com>
2023-03-19 16:49:20 +0530

    feat: adds more routes that require auth

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/campgrounds/new'],
+  matcher: ['/campgrounds/new', '/api/subscribe', '/api/campgrounds'],
   pages: {
     signIn: '/auth/login',
   },

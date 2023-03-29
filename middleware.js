297e1b749edc6321ce5617509f328750c102b589
dharmik48 <hingudharmik4@gmail.com>
2023-03-29 18:14:28 +0530

    fix: redirect to login if access campground confirm

--- middleware.js
+++ middleware.js
@@ -1,7 +1,12 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/campgrounds/new', '/api/subscribe', '/api/campgrounds'],
+  matcher: [
+    '/campgrounds/new',
+    '/api/subscribe',
+    '/api/campgrounds',
+    '/campgrounds/:id/confirm',
+  ],
   pages: {
     signIn: '/auth/login',
   },

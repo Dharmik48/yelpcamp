dff1241389786bb23f44df85e278887bc9d73510
dharmik48 <hingudharmik4@gmail.com>
2023-04-14 18:58:12 +0530

    feat: show msg if no notifs and reload on clear

--- pages/users/[id]/notifications.js
+++ pages/users/[id]/notifications.js
@@ -25,6 +25,7 @@ const Notifications = ({ notifications, autorized = false, user }) => {
   const clear = async () => {
     try {
       await axios.post('/api/notifications/clear', { user })
+      router.reload()
     } catch (e) {
       toast.error('Something went wrong')
     }
@@ -55,61 +56,65 @@ const Notifications = ({ notifications, autorized = false, user }) => {
         </div>
       </div>
       <ul className='flex flex-col gap-4'>
-        {notifications.map(noti => (
-          <li
-            key={noti._id}
-            className={`${
-              noti.read ? 'opacity-80' : 'opacity-100'
-            } flex items-center gap-4 rounded-lg bg-lightBlue p-4`}
-          >
-            <div className='hidden md:block'>
-              <Image
-                src={noti.campground.images[0].url}
-                width='225'
-                height='100'
-                className='h-full w-auto rounded-lg'
-                alt={`Image of ${noti.campground.name}`}
-              />
-            </div>
-            <div className='flex flex-col gap-2'>
-              <p className='text-sm text-text'>
-                {dayjs().to(dayjs(noti.createdAt))}
-              </p>
-              <p className='text-lg lg:text-xl'>
-                User{' '}
-                <Link
-                  href={`/users/${noti.user._id}`}
-                  className='font-volkhov text-brand'
-                >
-                  {noti.user.name}
-                </Link>{' '}
-                made a booking for{' '}
-                <Link
-                  href={`/campgrounds/${noti.campground._id}`}
-                  className='font-volkhov text-brand'
-                >
-                  {noti.campground.name}
-                </Link>
-              </p>
-              <p>
-                Guests:{' '}
-                {`${noti.guests.adults} adults${
-                  !!noti.guests.children
-                    ? ', ' + noti.guests.children + ' children'
-                    : ''
-                } ${
-                  !!noti.guests.infants
-                    ? ', ' + noti.guests.infants + ' infants'
-                    : ''
-                }`}
-              </p>
-              <div>
-                <p>Check In: {dayjs(noti.dates.checkIn).format('LL')}</p>{' '}
-                <p>Check Out: {dayjs(noti.dates.checkOut).format('LL')}</p>
+        {notifications.length ? (
+          notifications.map(noti => (
+            <li
+              key={noti._id}
+              className={`${
+                noti.read ? 'opacity-80' : 'opacity-100'
+              } flex items-center gap-4 rounded-lg bg-lightBlue p-4`}
+            >
+              <div className='hidden md:block'>
+                <Image
+                  src={noti.campground.images[0].url}
+                  width='225'
+                  height='100'
+                  className='h-full w-auto rounded-lg'
+                  alt={`Image of ${noti.campground.name}`}
+                />
               </div>
-            </div>
-          </li>
-        ))}
+              <div className='flex flex-col gap-2'>
+                <p className='text-sm text-text'>
+                  {dayjs().to(dayjs(noti.createdAt))}
+                </p>
+                <p className='text-lg lg:text-xl'>
+                  User{' '}
+                  <Link
+                    href={`/users/${noti.user._id}`}
+                    className='font-volkhov text-brand'
+                  >
+                    {noti.user.name}
+                  </Link>{' '}
+                  made a booking for{' '}
+                  <Link
+                    href={`/campgrounds/${noti.campground._id}`}
+                    className='font-volkhov text-brand'
+                  >
+                    {noti.campground.name}
+                  </Link>
+                </p>
+                <p>
+                  Guests:{' '}
+                  {`${noti.guests.adults} adults${
+                    !!noti.guests.children
+                      ? ', ' + noti.guests.children + ' children'
+                      : ''
+                  } ${
+                    !!noti.guests.infants
+                      ? ', ' + noti.guests.infants + ' infants'
+                      : ''
+                  }`}
+                </p>
+                <div>
+                  <p>Check In: {dayjs(noti.dates.checkIn).format('LL')}</p>{' '}
+                  <p>Check Out: {dayjs(noti.dates.checkOut).format('LL')}</p>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p>No new notifications</p>
+        )}
       </ul>
     </section>
   )

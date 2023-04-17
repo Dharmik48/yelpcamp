2bacec62d3e51d7f2b88ff2f0fc790a3ba23c9b1
dharmik48 <hingudharmik4@gmail.com>
2023-04-17 19:53:16 +0530

    feat: adds filters

--- components/ComboBox.js
+++ components/ComboBox.js
@@ -1,7 +1,13 @@
 import { Fragment, useEffect, useState } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 
-export default function ComboBox({ list, selected, setSelected }) {
+export default function ComboBox({
+  list,
+  styles,
+  selected,
+  setSelected,
+  listStyles = '-translate-y-full',
+}) {
   const [query, setQuery] = useState('')
 
   const filteredList =
@@ -18,7 +24,9 @@ export default function ComboBox({ list, selected, setSelected }) {
     <div>
       <Combobox value={selected} onChange={setSelected}>
         <div className='relative'>
-          <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300'>
+          <div
+            className={`relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 ${styles}`}
+          >
             <Combobox.Input
               className='w-full border-none focus:ring-0'
               displayValue={item => item.text}
@@ -32,7 +40,9 @@ export default function ComboBox({ list, selected, setSelected }) {
             leaveTo='opacity-0'
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options className='absolute top-0 mt-1 max-h-60 -translate-y-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+            <Combobox.Options
+              className={`absolute top-0 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm ${listStyles}`}
+            >
               {filteredList.length === 0 && query !== '' ? (
                 <div className='relative cursor-default select-none py-2 px-4 text-gray-700'>
                   Nothing found.
--- pages/campgrounds/index.js
+++ pages/campgrounds/index.js
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { getCampgrounds } from '../../util/campgrounds'
 import CampgroundCard from '../../components/CampgroundCard'
 // map stuff
 import ReactMapGl, { GeolocateControl, Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { FaMapMarked, FaListUl, FaThumbtack } from 'react-icons/fa'
+import {
+  FaMapMarked,
+  FaListUl,
+  FaThumbtack,
+  FaTimes,
+  FaCheck,
+} from 'react-icons/fa'
+import Campground from '../../models/Campground'
+import connectDB from '../../util/mongo'
+import Review from '../../models/Review'
+import User from '../../models/User'
+import { IoOptions } from 'react-icons/io5'
+import ComboBox from '../../components/ComboBox'
+import { getNames } from 'country-list'
+import { useRouter } from 'next/router'
 
-const Campgrounds = ({ campgrounds, search, location }) => {
+const Campgrounds = ({
+  campgrounds,
+  filters = { location: '', price_min: '', price_max: '' },
+  numOfFilters,
+}) => {
+  const router = useRouter()
+
+  const [showFilterMenu, setShowFilterMenu] = useState(false)
+
+  const [location, setLocation] = useState(filters.location)
+  const [minPrice, setMinPrice] = useState(filters.price_min)
+  const [maxPrice, setMaxPrice] = useState(filters.price_max)
+
+  const countries = getNames().map(country => ({
+    text: country,
+    disable: false,
+  }))
+
   // Map over the campgrounds to create JSX
   const renderCampgrounds = campgrounds.map(campground => (
     <li key={campground._id}>
@@ -29,6 +59,12 @@ const Campgrounds = ({ campgrounds, search, location }) => {
 
   const [showMap, setShowMap] = useState(false)
 
+  const handleFilterSubmit = () => {
+    router.push(
+      `/campgrounds?location=${location.text}&price_min=${minPrice}&price_max=${maxPrice}`
+    )
+  }
+
   return (
     <>
       <Head>
@@ -36,16 +72,77 @@ const Campgrounds = ({ campgrounds, search, location }) => {
       </Head>
       <section className='relative py-12'>
         <div className='mb-10 flex items-center justify-between'>
-          <h2 className='font-volkhov text-3xl'>
-            {search ? (
-              <>
-                Campgrounds in <span className='text-brand'>{location}</span>
-              </>
-            ) : (
-              'Top Campgrounds'
+          <h2 className='font-volkhov text-3xl'>Top Campgrounds</h2>
+          <div className='relative md:hidden'>
+            <button
+              className='relative flex items-center gap-2 rounded-md border-2 border-dark p-2 '
+              onClick={e => setShowFilterMenu(prev => !prev)}
+            >
+              <IoOptions />
+              <span>Filters</span>
+              {numOfFilters > 0 && (
+                <span className='absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 rounded-full bg-brand p-1 text-primaryBg'>
+                  {numOfFilters}
+                </span>
+              )}
+            </button>
+            {showFilterMenu && (
+              <form
+                className='absolute top-full right-0 z-10 mt-2 flex max-w-xs flex-col gap-3 rounded-lg bg-lightRed p-5'
+                onSubmit={handleFilterSubmit}
+              >
+                <div>
+                  <label htmlFor='location'>Location: </label>
+                  <ComboBox
+                    listStyles='right-0'
+                    list={[{ text: 'Select', disable: true }, ...countries]}
+                    selected={location}
+                    styles={'p-2'}
+                    setSelected={setLocation}
+                  />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <label htmlFor='min_price' className='flex-1'>
+                    Min Price:{' '}
+                  </label>
+                  <input
+                    type='number'
+                    id='min_price'
+                    onChange={e => setMinPrice(e.target.value)}
+                    className='w-36 rounded-lg p-2'
+                    value={minPrice}
+                  />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <label htmlFor='max_price' className='flex-1'>
+                    Max Price:{' '}
+                  </label>
+                  <input
+                    type='number'
+                    id='max_price'
+                    onChange={e => setMaxPrice(e.target.value)}
+                    className='w-36 rounded-lg p-2'
+                    value={maxPrice}
+                  />
+                </div>
+                <div className='mt-2 flex items-center justify-between'>
+                  <button
+                    type='button'
+                    className='flex items-center gap-1'
+                    onClick={() => router.push('/campgrounds')}
+                  >
+                    <FaTimes />
+                    Clear
+                  </button>
+                  <button type='submit' className='flex items-center gap-1'>
+                    <FaCheck />
+                    Apply
+                  </button>
+                </div>
+              </form>
             )}
-          </h2>
-          <div className='hidden md:block'>
+          </div>
+          <div className='hidden items-center gap-4 md:flex'>
             {showMap ? (
               <button
                 className='flex items-center gap-2 rounded-md bg-blue p-2 text-white'
@@ -63,6 +160,77 @@ const Campgrounds = ({ campgrounds, search, location }) => {
                 Show Map
               </button>
             )}
+            <div className='relative'>
+              <button
+                className='flex items-center gap-2 rounded-md border-2 border-dark p-2'
+                onClick={e => setShowFilterMenu(prev => !prev)}
+              >
+                <IoOptions />
+                <span>Filters</span>
+                {numOfFilters > 0 && (
+                  <span className='absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 rounded-full bg-brand p-1 text-primaryBg'>
+                    {numOfFilters}
+                  </span>
+                )}
+              </button>
+              {showFilterMenu && (
+                <form
+                  className='absolute top-full right-0 z-10 mt-2 flex max-w-xs flex-col gap-3 rounded-lg bg-lightRed p-5'
+                  onSubmit={handleFilterSubmit}
+                >
+                  <div>
+                    <label htmlFor='location'>Location: </label>
+                    <ComboBox
+                      listStyles='right-0'
+                      list={[{ text: 'Select', disable: true }, ...countries]}
+                      selected={location}
+                      styles={'p-2'}
+                      setSelected={setLocation}
+                    />
+                  </div>
+                  <div className='flex items-center gap-2'>
+                    <label htmlFor='min_price' className='w-max'>
+                      Min Price:{' '}
+                    </label>
+                    <input
+                      type='number'
+                      id='min_price'
+                      onChange={e => setMinPrice(e.target.value)}
+                      className='w-36 rounded-lg p-2'
+                      min='0'
+                      value={minPrice}
+                    />
+                  </div>
+                  <div className='flex items-center gap-2'>
+                    <label htmlFor='max_price' className='w-max'>
+                      Max Price:{' '}
+                    </label>
+                    <input
+                      type='number'
+                      id='max_price'
+                      onChange={e => setMaxPrice(e.target.value)}
+                      className='w-36 rounded-lg p-2'
+                      min='0'
+                      value={maxPrice}
+                    />
+                  </div>
+                  <div className='mt-2 flex items-center justify-between'>
+                    <button
+                      type='button'
+                      className='flex items-center gap-1'
+                      onClick={() => router.push('/campgrounds')}
+                    >
+                      <FaTimes />
+                      Clear
+                    </button>
+                    <button type='submit' className='flex items-center gap-1'>
+                      <FaCheck />
+                      Apply
+                    </button>
+                  </div>
+                </form>
+              )}
+            </div>
           </div>
           <div className='fixed bottom-8 right-8 z-40 sm:bottom-10 sm:right-10 md:bottom-12 md:right-12 md:hidden'>
             {showMap ? (
@@ -114,17 +282,59 @@ const Campgrounds = ({ campgrounds, search, location }) => {
 }
 
 export async function getServerSideProps(context) {
-  let { location } = context.query
-  location = !!location ? location : null
-  const search = !!location
+  await connectDB()
+  await Review.find({})
+  await User.find({})
+
+  const { location = null, price_min = null, price_max = null } = context.query
+
   // Fetch campground data
-  const campgrounds = await getCampgrounds({
-    filter: location,
-    sort: 'createdAt',
-  })
-  // Send the data as prop
+  const campgrounds = await Campground.find({})
+  const camps = JSON.parse(JSON.stringify(campgrounds))
+
+  if (!location && !price_min && !price_max)
+    return {
+      props: { campgrounds: camps, location, numOfFilters: 0 },
+    }
+
+  let numOfFilters = 0
+  let filteredCamps = camps
+
+  if (location) {
+    numOfFilters += 1
+    filteredCamps = filteredCamps.filter(
+      camp => camp.location.country === location
+    )
+  }
+
+  if (price_min) {
+    numOfFilters += 1
+    filteredCamps = filteredCamps.filter(
+      camp =>
+        (camp.price.adults * (100 - camp.price.discount)) / 100 >=
+        Number(price_min)
+    )
+  }
+
+  if (price_max) {
+    numOfFilters += 1
+    filteredCamps = filteredCamps.filter(
+      camp =>
+        (camp.price.adults * (100 - camp.price.discount)) / 100 <=
+        Number(price_max)
+    )
+  }
+
   return {
-    props: { campgrounds, search, location },
+    props: {
+      campgrounds: filteredCamps,
+      filters: {
+        location: { text: location },
+        price_min,
+        price_max,
+      },
+      numOfFilters,
+    },
   }
 }
 

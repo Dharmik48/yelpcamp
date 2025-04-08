332db7ae928cd52e6b5c996bd42329e5b6a866ac
Dharmik_403 <64529217+Dharmik48@users.noreply.github.com>
2025-04-08 21:30:50 +0530

    fix: replace check with svg

--- pages/campgrounds/[id]/index.js
+++ pages/campgrounds/[id]/index.js
@@ -16,8 +16,7 @@ import {
   FaDog,
   FaWifi,
   FaCampground,
-} from 'react-icons/fa'
-import { LuBadgeCheck } from "react-icons/lu"
+} from 'react-icons/fa"
 // import { TbCampfire } from 'react-icons/tb'
 import {
   GiWashingMachine,
@@ -178,7 +177,7 @@ const CampgroundDetail = ({ campground = camp, user = null }) => {
             <h1 className='mb-2 font-volkhov text-3xl text-brand lg:mb-4 lg:text-4xl 2xl:text-5xl'>
               {campground?.name}
             </h1>
-            {campground.plusExclusive && <LuBadgeCheck />}
+            {campground.plusExclusive && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check-icon lucide-badge-check"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m9 12 2 2 4-4"/></svg>}
           </div>
           <p className='text-sm'>
             {`${campground?.location.city}, ${campground?.location.state}, ${campground?.location.country}`}{' '}
